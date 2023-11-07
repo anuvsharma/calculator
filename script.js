b0e07fb98bdbdce9fed9f6216e775fda8dd8ddb1
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-11-06 19:51:06 -0800

    added basic operations and user interface
    created html, css, and javascript files, added basic functions to script.js. added buttons to index.html. applied basic styling to user interface in sytles.css file

---
+++ script.js
@@ -0,0 +1,34 @@
+let firstNumber;
+let operator;
+let secondNumber;
+
+function add(a, b){
+    return a+b;
+}
+
+function subtract(a, b){
+    return a-b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function operate(firstNumber, operator, secondNumber){
+    if (operator == 'add'){
+        return add(firstNumber,secondNumber);
+    }
+    else if (operator == 'subtract'){
+        return subtract(firstNumber,secondNumber);
+    }
+    else if (operator == 'multiply'){
+        return multiply(firstNumber,secondNumber);
+    }
+    else if (operator == 'divide'){
+        return divide(firstNumber,secondNumber);
+    }
+}
