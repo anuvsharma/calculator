b04e84470741d498f7bc59efc3f25b0305d4a042
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-11-08 18:42:34 -0800

    buttons pressed now show on current display. clear and delete buttons work.
    added functionality to the number buttons so that they display on the DOM. added functionality of the CLEAR and DELETE buttons as well.

--- script.js
+++ script.js
@@ -2,7 +2,44 @@ let firstNumber;
 let operator;
 let secondNumber;
 let displayValue;
+let numberButtons = document.querySelectorAll(".number-button");
+let currentDisplay = document.getElementById("current-display");
+let clearButton = document.getElementById("button-clear");
+let deleteButton = document.getElementById("button-delete");
 
+numberButtons.forEach(function(button) {
+    button.addEventListener("click", function(){
+        appendDisplay(button.textContent);
+    });
+});
+
+clearButton.addEventListener("click", function(){
+    appendDisplay("clear");
+})
+
+deleteButton.addEventListener("click", function(){
+    appendDisplay("delete");
+})
+
+function appendDisplay(number){
+    if(number == "clear"){
+        currentDisplay.textContent = 0;
+    }
+    else if(number == "delete"){
+        currentDisplay.textContent = currentDisplay.textContent.slice(0,-1);
+        if(currentDisplay.textContent == ""){
+            currentDisplay.textContent = 0;
+        }
+    }
+    else if(currentDisplay.textContent == 0){
+        currentDisplay.textContent = number;
+        console.log('pressed else if'+ number);
+    }
+    else{
+        currentDisplay.textContent += number;
+        console.log('pressed '+ number);
+    }
+}
 
 function add(a, b){
     return a+b;
