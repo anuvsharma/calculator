f03c26f48a64fa7ce4eae39d88ac5a69048409da
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-11-08 19:44:00 -0800

    adds functionality to all operations
    added functionality to addition, subtraction, multiplication and division operations. calculator will not evaluate more than a single pair at a time

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ let secondNumber;
 let displayValue;
 let numberButtons = document.querySelectorAll(".number-button");
 let currentDisplay = document.getElementById("current-display");
+let oldDisplay = document.getElementById("old-display");
 let clearButton = document.getElementById("button-clear");
 let deleteButton = document.getElementById("button-delete");
+let operatorButtons = document.querySelectorAll(".operator-button");
+let equalsButton = document.getElementById("button-equals");
 
 numberButtons.forEach(function(button) {
     button.addEventListener("click", function(){
@@ -21,9 +24,21 @@ deleteButton.addEventListener("click", function(){
     appendDisplay("delete");
 })
 
+operatorButtons.forEach(function(button) {
+    button.addEventListener("click", function(){
+        setOperator(button.textContent);
+    });
+});
+
+equalsButton.addEventListener("click", function(){
+    currentDisplay.textContent = operate(oldDisplay.textContent.slice(0,-2), operator, currentDisplay.textContent);
+    oldDisplay.textContent = "";
+})
+
 function appendDisplay(number){
     if(number == "clear"){
         currentDisplay.textContent = 0;
+        oldDisplay.textContent = "";
     }
     else if(number == "delete"){
         currentDisplay.textContent = currentDisplay.textContent.slice(0,-1);
@@ -33,41 +48,86 @@ function appendDisplay(number){
     }
     else if(currentDisplay.textContent == 0){
         currentDisplay.textContent = number;
-        console.log('pressed else if'+ number);
     }
     else{
         currentDisplay.textContent += number;
-        console.log('pressed '+ number);
+    }
+}
+
+function setOperator(operation){
+    if(operation == "+"){
+        if(oldDisplay.textContent != "" && currentDisplay.textContent != ""){
+            currentDisplay.textContent = operate(oldDisplay.textContent.slice(0,-2), operator, currentDisplay.textContent);
+            oldDisplay.textContent = "";
+        }
+        operator = operation;
+        oldDisplay.textContent = currentDisplay.textContent + " " + operation;
+        currentDisplay.textContent = 0;
+    }
+    else if (operation == "-"){
+        if(oldDisplay.textContent != "" && currentDisplay.textContent != ""){
+            currentDisplay.textContent = operate(oldDisplay.textContent.slice(0,-2), operator, currentDisplay.textContent);
+            oldDisplay.textContent = "";
+        }
+        operator = operation;
+        oldDisplay.textContent = currentDisplay.textContent + " " + operation;
+        currentDisplay.textContent = 0;
+    }
+    else if (operation == "×"){
+        if(oldDisplay.textContent != "" && currentDisplay.textContent != ""){
+            currentDisplay.textContent = operate(oldDisplay.textContent.slice(0,-2), operator, currentDisplay.textContent);
+            oldDisplay.textContent = "";
+        }
+        operator = operation;
+        oldDisplay.textContent = currentDisplay.textContent + " " + operation;
+        currentDisplay.textContent = 0;
+    }
+    else if (operation == "÷"){
+        if(oldDisplay.textContent != "" && currentDisplay.textContent != ""){
+            currentDisplay.textContent = operate(oldDisplay.textContent.slice(0,-2), operator, currentDisplay.textContent);
+            oldDisplay.textContent = "";
+        }
+        operator = operation;
+        oldDisplay.textContent = currentDisplay.textContent + " " + operation;
+        currentDisplay.textContent = 0;
     }
 }
 
 function add(a, b){
-    return a+b;
+    return parseFloat(a)+parseFloat(b);
 }
 
 function subtract(a, b){
-    return a-b;
+    return parseFloat(a)-parseFloat(b);
 }
 
 function multiply(a, b){
-    return a*b;
+    return parseFloat(a)*parseFloat(b);
 }
 
 function divide(a, b){
-    return a/b;
+    if (b == 0){
+        appendDisplay("clear");
+        alert("Can't divide by 0!");
+        return 0;
+    }
+    return parseFloat(a)/parseFloat(b);
 }
 
 function operate(firstNumber, operator, secondNumber){
-    if (operator == 'add'){
+    if (operator == '+'){
+/*         console.log(firstNumber);
+        console.log(operator);
+        console.log(secondNumber); */
         return add(firstNumber,secondNumber);
     }
-    else if (operator == 'subtract'){
+    else if (operator == '-'){
         return subtract(firstNumber,secondNumber);
     }
-    else if (operator == 'multiply'){
+    else if (operator == '×'){
         return multiply(firstNumber,secondNumber);
     }
-    else if (operator == 'divide'){
+    else if (operator == '÷'){
         return divide(firstNumber,secondNumber);
     }
 }
